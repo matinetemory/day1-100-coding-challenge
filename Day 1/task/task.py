20956205651fb1e513877e65c2c0a28db523c20e
matinetemory <intlbowg@gmail.com>
2026-05-23 15:54:39 -0400

    bandnamegenerator

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello Welcome to the Band Name Generator")
+city = input("What city did you grow up in?")
+pet_name = input("What is your pet name?")
+print("Your band name is " + city + " " + pet_name)
