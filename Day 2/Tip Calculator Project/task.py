1b1862cdeaf7351bdb84e1f0a5261b2539392e66
matinetemory <intlbowg@gmail.com>
2026-05-24 13:34:22 -0400

    day1-100 coding

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,10 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+price = bill * (tip / 100)
+total = bill + price
+per_person = bill / people
+cost = round(per_person, 2)
+
+print("Each person should pay $" + str(cost))
 
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,1 @@
-len(12345)
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
