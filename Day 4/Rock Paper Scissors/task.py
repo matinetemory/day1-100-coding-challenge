4d786e3e09f2f792c759ef7ddd40fcc859c0ba17
matinetemory <intlbowg@gmail.com>
2026-05-25 21:27:47 -0400

    day1-100 coding

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,40 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors ")
+
+if choice == "0":
+    print(rock)
+elif choice == "1":
+    print(paper)
+elif choice == "2":
+    print(scissors)
+else:
+    print("Wrong Choice ")
+
+print ("Computer Choose:")
+list_of_choices = [rock, paper, scissors]
+computer_choice = (random.choice(list_of_choices))
+print(computer_choice)
+
+if computer_choice == rock and choice == "0":
+    print("Draw")
+elif computer_choice == rock and choice == "1":
+    print("You lose")
+elif computer_choice == rock and choice == "2":
+    print("You win")
+elif computer_choice == paper and choice == "0":
+    print("You lose")
+elif computer_choice == paper and choice == "1":
+    print("Draw")
+elif computer_choice == paper and choice == "2":
+    print("You win")
+elif computer_choice == scissors and choice == "0":
+    print("You lose")
+elif computer_choice == scissors and choice == "1":
+    print("You win")
+elif computer_choice == scissors and choice == "2":
+    print("Draw")
+else:
+    print("Something went wrong")
+
